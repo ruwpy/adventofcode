964dc559929939ba27ea47de8e306f447016e608
ruwpy <ruwpy.who@gmail.com>
2023-12-02 21:47:52 +0100

    completed day one part one

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use crate::parse_txt::parse_txt;
+use crate::puzzles::day_one;
+
+mod parse_txt;
+mod puzzles;
+
+fn main() -> io::Result<()> {
+    let input = parse_txt("src/puzzles/day_one/input.txt")?;
+
+    let result = day_one::part_one::trebuchet(&input);
+    println!("{}", result);
+
+    Ok(())
 }
